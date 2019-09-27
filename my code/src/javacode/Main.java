f87491410b08a983358fa4f617e673f04620971f
anjireddy1624 <yagnamanji424@gmail.com>
2019-09-27 13:22:57 +0530

    first commit uploading all files

---
+++ my code/src/javacode/Main.java
@@ -0,0 +1,11 @@
+package javacode;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello java how are you");
+		
+
+	}
+
+}
---
+++ my code/src/javacode/Second.java
@@ -0,0 +1,14 @@
+package javacode;
+
+public class Second {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=345;
+		b=543;
+		c=a+b;
+		System.out.println("print="+c);
+
+	}
+
+}
