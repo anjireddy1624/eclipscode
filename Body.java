9213293f141980d471b48a8fa0267cbed277cd34
anjireddy1624 <yagnamanji424@gmail.com>
2019-10-16 10:29:06 +0000

    three

---
+++ Body.java
@@ -0,0 +1,14 @@
+class Body
+{
+	public static void main(String[] ad)
+	{
+		for(in i=0;i<n;i++)
+		{
+			for(int j=0;i<=j;j++)
+			{
+			System.out.print("*");
+			}
+		System.out.println("");
+		}
+	}
+}	
