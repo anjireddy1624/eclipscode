d48438e3533f712ce66387077b0cb01ce2ccca34
anjireddy1624 <yagnamanji424@gmail.com>
2019-10-16 10:46:29 +0000

    three changes

--- Body.java
+++ Body.java
@@ -2,13 +2,14 @@ class Body
 {
 	public static void main(String[] ad)
 	{
-		for(in i=0;i<n;i++)
+		int n=5;
+		for(int i=0;i<n;i++)
 		{
 			for(int j=0;i<=j;j++)
 			{
 			System.out.print("*");
 			}
-		System.out.println("");
+		System.out.println( );
 		}
 	}
 }	
